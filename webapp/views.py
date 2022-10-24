01408a7f324503b9151cb57f2ad47a9754c71f5e
senttttt <s1f102002840@iniad.org>
2022-10-25 00:25:21 +0900

    change HTML css

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from webapp.models import App
 
 # Create your views here.
+def index(request):
+    return render(request,'webapp/index.html')
+
+def search(request):
+    school = request.POST['schoolInput']
+    subject = request.POST['subjectInput']
+    money = request.POST['moneyInput']
+    value = request.POST['valueInput']
+    machine = request.POST['machineInput']
+    app = App.objects.filter(subject__contains = subject,school__contains = school,money__lte = money,value__gte = value,machine__contains = machine)
+    context = {
+        'app':app,
+    }
+    return render(request,"webapp/search.html",context)
